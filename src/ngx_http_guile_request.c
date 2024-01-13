935e41f0209e0ddb3cd3338241eafe02d4bb6f3b
tomrss <tommaso.rossi1@protonmail.com>
2024-01-13 12:44:14 +0100

    Add copyright notice

--- src/ngx_http_guile_request.c
+++ src/ngx_http_guile_request.c
@@ -1,3 +1,22 @@
+/* Guile NGINX Module
+ * Copyright (C) 2023-2024 Tommaso Rossi
+ *
+ * This file is part of Guile NGINX Module.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
 #include "ngx_http_guile_request.h"
 
 // TODO dangerous global, move it in configuration scope
